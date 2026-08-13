9dcb4acd41617d8c0c0fc7089b293769a64e2df7
thirunagarivarsha-bit <thirunagari.varsha@klh.edu.in>
2026-08-13 15:13:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -449,4 +449,4 @@ def seed_demo():
 seed_demo()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
+   app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=False)
